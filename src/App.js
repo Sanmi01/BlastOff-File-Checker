cbf818dfc6cb725e6a576192b913331010bdba52
Sanmi01 <supersanmi01@gmail.com>
2022-05-03 23:39:35 +0100

    implemented functionalities

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import * as XLSX from 'xlsx';
+import Dropzone from 'react-dropzone';
+
+
 
 function App() {
+  const filesWithErrors = [];
+  const [renderedFiles, setRenderedFiles] = useState([]);
 
-  const checkAnswer = (data) => {
+  const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+  };
+
+  const [style] = useState(baseStyle)
+
+  const checkAnswer = (data, fileWithError) => {
+    let answers = []
     data.forEach((item, index) => {
       if((item.Answer === item.A) || (item.Answer === item.B) ||(item.Answer === item.C) ||(item.Answer === item.D)) {
       } else {
-        console.log("Answer does not match Option")
+        answers.push({...item, Error: "Answer is not among Options"})
+        // console.log("Answer does not match Option")
+        // console.log(item)
       }
     });
+
+    fileWithError.answerErrors = answers
   }
 
-  const checkDuplicateOptions = (data) => {
+  const checkDuplicateOptions = (data, fileWithError) => {
+    let duplicates = []
     data.forEach((item, index) => {
       if((item.A === item.B) || (item.A === item.C) || (item.A === item.D) || (item.B === item.C) || (item.B === item.D) || (item.C === item.D)) {
-        console.log("Duplicate Options")
-        console.log(index)
+        duplicates.push({...item, Error: "Duplicate Options"})
+        // console.log("Duplicate Options")
+        // console.log(item)
       }
     })
+
+    fileWithError.duplicateErrors = duplicates 
   }
 
   const readExcel = (file) => {
+    
+    let fileWithError = {};
+    fileWithError.name = file.name
 
     const promise = new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -49,11 +84,17 @@ function App() {
     });
 
     promise.then((data) => {
-      console.log(data)
-      checkAnswer(data)
-      checkDuplicateOptions(data)
-    
+      // console.log(data)
+      checkAnswer(data, fileWithError)
+      checkDuplicateOptions(data, fileWithError)
+      if(fileWithError.answerErrors.length === 0 && fileWithError.duplicateErrors.length === 0) {
+
+      } else {
+        filesWithErrors.push(fileWithError)
+      }
+      
     })
+    
   }
 
   return (
@@ -64,6 +105,24 @@ function App() {
           readExcel(file);
         }} />
       </div>
+      <Dropzone onDrop={(acceptedFiles) => {
+      acceptedFiles.forEach((item, index) => {
+      readExcel(item)
+      
+    })
+    setRenderedFiles(filesWithErrors)
+    console.log(filesWithErrors)
+    }}
+      >
+  {({getRootProps, getInputProps}) => (
+    <section>
+      <div {...getRootProps({style})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </section>
+  )}
+</Dropzone>
     </div>
   );
 }
